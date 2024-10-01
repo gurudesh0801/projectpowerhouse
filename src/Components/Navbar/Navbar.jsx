1a1009c176346aed9e9a55599178e9710d91a8e8
gurudesh0801 <111891244+gurudesh0801@users.noreply.github.com>
2024-10-01 11:11:17 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,31 +10,34 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <img src={logo} alt="" />
-        Project Powerhouse
-      </div>
-      <div className={`menu ${isOpen ? "active" : ""}`}>
-        <a href="#landingpage" className="nav-link">
-          Home
-        </a>
-        <a href="#about" className="nav-link">
-          About Us
-        </a>
-        <a href="#services" className="nav-link">
-          Services
-        </a>
-        <a href="#contact" className="nav-link">
-          Contact
-        </a>
-      </div>
-      <div className="hamburger" onClick={toggleMenu}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="logo">
+          <img src={logo} alt="" />
+          Project Powerhouse
+        </div>
+        <div className={`menu ${isOpen ? "active" : ""}`}>
+          <a href="#landingpage" className="nav-link">
+            Home
+          </a>
+          <a href="#about" className="nav-link">
+            About Us
+          </a>
+          <a href="#services" className="nav-link">
+            Services
+          </a>
+          <a href="#contact" className="nav-link">
+            Contact
+          </a>
+        </div>
+        <div className="hamburger" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </nav>
+      <hr />
+    </>
   );
 };
 
