54269aa11189b676db14aab24504dd498665328f
gurudesh0801 <111891244+gurudesh0801@users.noreply.github.com>
2024-10-07 15:45:33 +0530

    Update LandingPage.jsx

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 // src/components/LandingPage.js
 import React from "react";
 import "./LandingPage.css";
-import img1 from "../../assets/images/img1.webp";
+import img1 from "../../assets/images/bgimg4.png";
 import {
   FaHtml5,
   FaCss3Alt,
@@ -27,14 +27,18 @@ const LandingPage = () => {
       </div>
 
       <div className="hero-content">
-        <h1>We Are the Future of Tech Innovation</h1>
+        <h2>Welcome to</h2>
+        <h1>Project Powerhouse</h1>
         <p>
           Our commitment to pushing the boundaries of technology. By integrating
           cutting-edge solutions and forward-thinking strategies, we empower
           businesses to thrive in an ever-evolving digital landscape
         </p>
         <a href="#contact" className="cta-button">
-          Book a Call
+          Contact
+        </a>
+        <a href="#contact" className="cta-button">
+          Book a Demo
         </a>
       </div>
       <div className="hero-image">
